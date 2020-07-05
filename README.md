e976b6ba57bb6077852772e123b5c750fe3686b3
mylidluk <mylidluk@gmail.com>
2020-07-05 01:32:42 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+minikube config set memory 16384
+minikube config set cpus 4
+minikube start
---
+++ src/logging-app/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const SimpleNodeLogger = require('simple-node-logger');
+const opts = {
+  timestampFormat:'YYYY-MM-DD HH:mm:ss.SSS'
+};
+const log = SimpleNodeLogger.createSimpleLogger(opts);
+
+(function repeatMe() {
+  setTimeout(() => {
+    log.info('it works');
+    repeatMe();
+  }, 1000)
+})();
